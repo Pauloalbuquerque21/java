9fe9104d1ca8d8e388599fa8a694ed60e57c5f05
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-02 08:56:16 -0300

    exercicio 1 terminado

--- untitled/src/exercicios_secton4/exercicios1.java
+++ untitled/src/exercicios_secton4/exercicios1.java
@@ -1,4 +1,20 @@
 package exercicios_secton4;
+import java.util.Scanner;
 
 public class exercicios1 {
+    public static void main(String[] args) {
+
+        Scanner leituraTeclado = new Scanner(System.in);
+
+        //conseguir as variaveis
+        System.out.println("Variavel 1:");
+        int variavel1 = leituraTeclado.nextInt();
+        System.out.println("Variavel 2:");
+        int variavel2 = leituraTeclado.nextInt();
+
+        //operação
+        double resultado = variavel1 + variavel2;
+
+        System.out.println("resultado soma: "+resultado);
+    }
 }
