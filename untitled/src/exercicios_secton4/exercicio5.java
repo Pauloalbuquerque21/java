780ff03ca4c1c56cb23cbd2ba50637fc9be8bf02
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-03 08:20:02 -0300

    iniciando a aula de loops

---
+++ untitled/src/exercicios_secton4/exercicio5.java
@@ -0,0 +1,19 @@
+package exercicios_secton4;
+import java.util.Scanner;
+public class exercicio5 {
+    public static void main(String[] args) {
+        Scanner leituraTeclado = new Scanner(System.in);
+
+
+        //Variavel
+        System.out.println("Digite o valor em Celsius:");
+        double celsius = leituraTeclado.nextDouble();
+
+        //logica para transforma
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println("Celsius: "+celsius+" é igual a "+fahrenheit+"Fahrenheit");
+
+
+    }
+}
