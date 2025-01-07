0aa70310b0ca89617ab9242c3628841f91838615
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-07 07:48:51 -0300

    indo para aula 30

--- untitled/src/loop/ContagemLetras.java
+++ untitled/src/loop/ContagemLetras.java
@@ -6,11 +6,16 @@ public class ContagemLetras {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite uma frase:");
 
+
         String frase = scanner.nextLine();
         int quantidadeLetras = 0;
-
-        for (int indice = 0; indice < frase.length() ; indice++){
+        //Usamos o método ".length()" para conseguir o tamanho da array ou string
+        for (int indice = 0 ; indice < frase.length() ; indice++){
+            //O método charAT é da class String, e tem como objetico ver a posição do caracter na string
+            //char:é um tipo primitivo que armazena caracteres
             char letra = frase.charAt(indice);
+            //Class Character é um class para o tipo char e tem vários métodos, e uma deles
+            // é o isLetter(char c), tem como objetivo verificar se é uma letra
             boolean isLetra = Character.isLetter(letra);
 
             if(isLetra){
@@ -18,6 +23,6 @@ public class ContagemLetras {
             }
         }
 
-        System.out.println("A quantidade de letras é:"+quantidadeLetras);
+        System.out.println("A quantidade de letras é:" + quantidadeLetras);
     }
 }
