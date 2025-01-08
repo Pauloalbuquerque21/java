d5e5c03a07f5863014c2f8bbd935e71c28e3c70c
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-08 09:27:00 -0300

    em processo com o exercício 9

---
+++ untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio9.java
@@ -0,0 +1,13 @@
+package Loops_LacosWithWhile_For.exercicios;
+import java.util.Scanner;
+
+public class exercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite o ano atual:");
+        int valor = scanner.nextInt();
+    }
+
+}
