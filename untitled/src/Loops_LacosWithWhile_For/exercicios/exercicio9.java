a6f848b7d09552204417fe18715d3554e6a38370
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-09 09:14:46 -0300

    terminei atividade 9

--- untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio9.java
+++ untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio9.java
@@ -8,6 +8,22 @@ public class exercicio9 {
 
         System.out.println("Digite o ano atual:");
         int valor = scanner.nextInt();
+        int result = 0;
+
+        if(valor % 4 == 0){
+            result += 1;
+            if (valor % 100 == 0){
+                result +=2;
+                if (valor % 400 == 0){
+                    result +=3;
+                }
+            }
+        }
+        if ((result == 1) || (result == 6) || (result == 4)){
+            System.out.println("Ano:"+valor+" ele é bissexto.");
+        }
+
+        scanner.close();
     }
 
 }
