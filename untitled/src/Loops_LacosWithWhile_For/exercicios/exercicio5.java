2578f09f9da5f822f4d529aee99d9b4234eaf101
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-09 09:48:18 -0300

    terminar a atividade 5 e ir para proxima etápa

---
+++ untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio5.java
@@ -0,0 +1,18 @@
+package Loops_LacosWithWhile_For.exercicios;
+import java.util.Scanner;
+public class exercicio5 {
+    public static void main(String[] args) {
+
+        int valor = 0;
+        while(valor <= 100) {
+            valor += 1;
+            if (valor % 3 == 0){
+
+            }else if(valor ){
+
+            }
+            System.out.println(valor);
+            System.out.println(valor);
+        }
+    }
+}
