6bbca9eb678d846ba819871c3dda9eb2c6beab1b
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-10 08:30:58 -0300

    atividade terminada

--- untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio5.java
+++ untitled/src/Loops_LacosWithWhile_For/exercicios/exercicio5.java
@@ -1,18 +1,21 @@
 package Loops_LacosWithWhile_For.exercicios;
-import java.util.Scanner;
 public class exercicio5 {
     public static void main(String[] args) {
 
         int valor = 0;
         while(valor <= 100) {
             valor += 1;
-            if (valor % 3 == 0){
-
-            }else if(valor ){
-
+            //condição de and no java é &&
+            if ((valor % 3 == 0) && (valor % 5 !=0)){
+                System.out.println("Fizz");
+            }else if((valor % 5 == 0) && (valor % 3 != 0)){
+                System.out.println("Buzz");
+            }else if ((valor % 5 == 0) && (valor % 3 == 0)){
+                System.out.println("FizzBuzz");
+            }else{
+                System.out.println(valor);
             }
-            System.out.println(valor);
-            System.out.println(valor);
+
         }
     }
 }
