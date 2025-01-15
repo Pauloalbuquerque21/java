028b0dabcc3797e2958cf66d6b7e86d4bea1f96c
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-14 23:20:09 -0300

    terminei a atividade 4, parti para estudar novas matérias

--- untitled/src/section6/exercicios/exercicio4.java
+++ untitled/src/section6/exercicios/exercicio4.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 
 public class exercicio4 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o seu salário:");
+        float valor = scanner.nextInt();
+        System.out.println("--------------------");
+        if (valor >= 2000){
+            System.out.println("Vai ter importo");
+            float resultado;
+            //Não esquece de colocar esse "f" apos o número, para não dar erro.
+            resultado = valor * 0.15f;
+            valor = valor - resultado;
+            System.out.println("O valor do desconto é "+resultado+" o valor do salário com o desconto é "+valor);
+        }else{
+            System.out.println("Não terá imposto ");
+        }
     }
 }
