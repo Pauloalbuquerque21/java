1c23c0cf8a59ea39b4701bec581d3a9a4eabaf3e
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-16 09:17:25 -0300

    atividade 2

--- untitled/src/section7/exercicios/exercicio2.java
+++ untitled/src/section7/exercicios/exercicio2.java
@@ -2,12 +2,21 @@ package section7.exercicios;
 import  java.util.Scanner;
 public class exercicio2 {
     public static void main(String[] args) {
+        //Importamos a class Scanner e transofarmos a variavel scanner para usar para captura as informações do teclado.
         Scanner scanner = new Scanner(System.in);
-
-
         String[] nomes = new String[6];
-        nomes = scanner.nextLine();
+
+        //Um laço for, para poder incluir as informações o array nomes
+        for(int nome = 0;nome < nomes.length;nome++){
+            nomes[nome] = scanner.nextLine();
+        }
+        //agora imprimir os nomes
+
+        for(int nome = 5;nome >= 0;nome = nome - 1){
+            System.out.println(nomes[nome]);
+        }
+
         }
 
     }
-}
+
