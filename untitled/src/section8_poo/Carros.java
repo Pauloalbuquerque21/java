3352c7b5b4cba0191d65e74e532854ca76450386
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-01-17 10:00:00 -0300

    Terminei a aula 47

--- untitled/src/section8_poo/Carros.java
+++ untitled/src/section8_poo/Carros.java
@@ -6,4 +6,22 @@ public class Carros {
     String transmissao;
     String montadora;
     String modelo;
+    int capacidadeTanque;
+    int combustivel;
+    int kmPorLitros;
+
+    //Metodo:comportamento da class
+    //Esse metódo, seria para abastecer
+    void abastecer(int litros){
+        combustivel = combustivel + litros;
+    }
+
+    //Esse método ele retorna um valor, logo temos que colocar o tipo, por isso que coloquei "int"
+    int autonomiaCombustível(){
+        int autonomia = combustivel * kmPorLitros;
+        return autonomia;
+    }
+
+
+
 }
--- untitled/src/section8_poo/MainCarros.java
+++ untitled/src/section8_poo/MainCarros.java
@@ -5,21 +5,35 @@ public class MainCarros {
     public static void main(String[] args) {
         //A tipo é a class Carros
         Carros punto = new Carros();
+
         // atributo cavalos recebe o valor 100, o atributo cavalos da variavel punto é 100
         punto.cavalos = 100;
         punto.cor = "Preta";
         punto.montadora = "Fiat";
         punto.transmissao = "Automatica";
+        punto.combustivel = 20;
         punto.modelo = "punto";
+
 
         Carros civic = new Carros();
         civic.cavalos = 150;
         civic.cor = "Prata";
         civic.montadora = "Honda";
         civic.transmissao = "Manual";
+        civic.combustivel = 0;
+        civic.modelo = "civic";
 
 
         System.out.println("Dados do punto:");
         System.out.println("O Carro: "+punto.modelo+" tem "+punto.cavalos+" cavalos.");
+
+        //Método abastecimento, para encher o tanque.
+        civic.abastecer(30);
+
+        System.out.println("O Carro: "+civic.modelo+" tem de combustível "+civic.combustivel);
+        System.out.println("O Carro: "+punto.modelo+" tem de combustível "+punto.combustivel);
+
+        //Método para mostrar a autonomia
+        System.out.println("O Carro: "+punto.modelo+" tem de autonomia "+punto.autonomiaCombustível());
     }
 }
