4eb64102e3348de3dea340a41cbbc005e52db298
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-02-16 10:21:34 -0300

    em processo

--- untitled1/src/br/com/paulo/heranca/main.java
+++ untitled1/src/br/com/paulo/heranca/main.java
@@ -1,12 +1,19 @@
 package br.com.paulo.heranca;
 
 public class main {
+    public static void main(String[] args) {
+        Panda panda = new Panda();
 
-    Panda panda = new Panda();
+        panda.setNome("Panda");
 
-    panda.setNome("Panda");
+        System.out.println(panda.getNome());
 
-    System.out.println(panda.getNome());
+        Hipopotamo hipopotamo = new Hipopotamo();
 
+        hipopotamo.setNome("Hipopotamo");
+
+        System.out.println(hipopotamo.getNome());
+
+    }
 
 }
