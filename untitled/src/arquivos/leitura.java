25f59e65608e060ff3755adf6d0f4b7b1d3bdc4e
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-04-18 11:14:38 -0300

    aprendendo leitura de arquivos

--- untitled/src/arquivos/leitura.java
+++ untitled/src/arquivos/leitura.java
@@ -1,8 +1,15 @@
 package arquivos;
+import java.io.*;
 
 public class leitura {
     public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\paulo\\OneDrive\\Documentos\\testando.txt"));
 
+        String texto = br.readline();
+
+        System.out.println(texto);
+
+        br.close();
 
 
 
