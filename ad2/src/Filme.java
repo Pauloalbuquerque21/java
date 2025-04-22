cdf38cd06783c05d7872b00d5aafe4185eff1eb4
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-04-22 08:07:38 -0300

    em processo

---
+++ ad2/src/Filme.java
@@ -0,0 +1,15 @@
+public class Filme {
+    private String titulo;
+    private String genero;
+    private int duracao;
+
+    public Filme(String titulo, String genero, int duracao){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+
+    }
+
+
+
+}
--- ad2/src/Main.java
+++ ad2/src/Main.java
@@ -1,7 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leituraTeclado = new Scanner(System.in);
+        //Entrada para quantas plataformas
+        System.out.print("Quantas plataformas serão cadastradas?");
+        int quantidade_catalogo = leituraTeclado.nextInt();
+
+        //Array com os arquvis
+        String[] filmes = new String[quantidade_catalogo];
+
+        //Entrada do nome do arquivo
+        for(int i = 0;i< filmes.length; i++) {
+            System.out.print("Qual o nome do arquivo:");
+            leituraTeclado.nextLine();
+            String nomeArquivo = leituraTeclado.nextLine();
+            filmes[i] = nomeArquivo;
+        }
+        BufferedReader plataformaTodo = null;
+
+        try {
+            for(int i = 0;i<filmes.length;i++){
+                FileReader arquivoPlataforma = new FileReader(filmes[i]);
+                plataformaTodo = new BufferedReader(arquivoPlataforma);
+                String linhaPlataforma = plataformaTodo.readLine();
+                System.out.println(linhaPlataforma);
+                while(linhaPlataforma != null){
+                    linhaPlataforma = plataformaTodo.readLine();
+                    System.out.println(linhaPlataforma);
+                    }
+            }
+
+        } catch(Exception erro){
+            //Vai tratar o erro.
+            System.out.print("Deu ruim");
+            System.out.println(erro.getMessage());
+        }
+
+
+
 
 
     }
