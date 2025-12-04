7b78f4f296b0e824e3b314c7e80f5a84e8ba527f
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-03 22:38:06 -0300

    reiniciando

--- Atividades gpt/media_vetor.java
+++ Atividades gpt/media_vetor.java
@@ -14,6 +14,7 @@ public class media_vetor{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         //Números solicitado para o usuário 
+        int[] arquivo= new int[10];
         System.out.println("Digite 10 dígitos:");
         
         int valor = 1;
@@ -23,12 +24,18 @@ public class media_vetor{
             
             System.out.println("Digite o valor "+valor+":");
             int valor_armazenado = scanner.nextInt();
+            arquivo[valor] = valor_armazenado;
             valor = valor + 1;
             System.out.println(valor_armazenado);
-
+            
 
             
         }
+
+        for (int i=0;i<arquivo.length;i++) {
+            System.out.println(arquivo[i]);
+        }
+
 
 
 
