e2c9e52aaca27e4fe7f64948587a68def98f2ee5
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-06 19:29:18 -0300

    terminei a segunda atividade

--- curso_java/Atividades gpt/procurar_elemento.java
+++ curso_java/Atividades gpt/procurar_elemento.java
@@ -6,15 +6,18 @@ public class procurar_elemento {
         int[] arquivo = new int[15];
 
         Random random = new Random();
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
         
+        //Definir variavel
+        int resultado = 0;
+
         //Cria os números e inclui no vetor
         for (int i=0;i<15;i++){
             int numero = random.nextInt(10);
             System.out.println(numero);
             arquivo[i] = numero;
         }
-        int resultado = 0;
+
         while(resultado != 2){
             System.out.println("Digite o valor:");
             int numero_usuario = scanner.nextInt();
@@ -23,14 +26,14 @@ public class procurar_elemento {
             //verifica o número do usuário com cada elemento que consta no vetor
             for (int i=0;i<15;i++){
                 if(arquivo[i]==numero_usuario){
-                    System.out.println("Número:"+numero_usuario+" encontrado");
-                    int resultado = 1;
+                    resultado = 1;
                 }
             }
 
             //
             if(resultado== 1){
                 resultado = resultado + 1;
+                System.out.println("Número: "+numero_usuario+" encontrado");
             }else{
                 System.out.println("Você digitou o número errado");
             }
