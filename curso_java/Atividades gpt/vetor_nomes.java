819642e1a9401fce96ac8b55325c45500dcaf5e5
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-08 10:10:08 -0300

    termine atividade 3

--- curso_java/Atividades gpt/vetor_nomes.java
+++ curso_java/Atividades gpt/vetor_nomes.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class vetor_nomes {
     public static void main(String[] args){
-        String[] nome = {"Maria","Carlos","Adriane","Matheos","Paulo"};
+        String[] nome = {"Maria","Carlos","Adriane","Marheos","Paulo"};
 
         Scanner scanner = new Scanner(System.in);
 
@@ -12,7 +13,24 @@ public class vetor_nomes {
 
         System.out.println("Segue a informação digitada do usuário: "+inform_user);
 
-        System.out.println(inform_user.length);
+        System.out.println(inform_user.length());
+
+        ArrayList<String> nomes_lista = new ArrayList<>();
+
+        for (int i=0;i<nome.length;i++){
+            int accountant = 0;
+            for (int e=0;e<inform_user.length();e++){
+                if(nome[i].charAt(e)==inform_user.charAt(e)){
+                    accountant = accountant + 1;
+                }
+            }
+            if(inform_user.length()==accountant){
+                nomes_lista.add(nome[i]);
+            }
+        }
+
+        System.out.println(nomes_lista);
+
 
 
         int tamanho = nome.length;
