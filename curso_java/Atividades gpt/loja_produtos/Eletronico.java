b4b2ab26fc7000849ace14c3e45dc3bec32b1325
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-23 18:39:02 -0300

    terminei

--- curso_java/Atividades gpt/loja_produtos/Eletronico.java
+++ curso_java/Atividades gpt/loja_produtos/Eletronico.java
@@ -1,4 +1,14 @@
 package loja_produtos;
 
-public class Eletronico {
+public class Eletronico extends Produto{
+
+    public Eletronico(String nome, double preco){
+        super(nome, preco);
+    }
+
+    @Override
+    public double calcularDesconto(){
+        double desconto = (super.preco * 8) / 100;
+        return super.preco - desconto;
+    }
 }
--- curso_java/Atividades gpt/loja_produtos/Livro.java
+++ curso_java/Atividades gpt/loja_produtos/Livro.java
@@ -1,4 +1,15 @@
 package loja_produtos;
 
-public class Livro {
+public class Livro extends Produto{
+
+
+    public Livro(String nome, double preco){
+        super(nome, preco);
+    }
+
+    @Override
+    public double calcularDesconto(){
+        double desconto = (super.preco * 15) / 100;
+        return super.preco - desconto;
+    }
 }
