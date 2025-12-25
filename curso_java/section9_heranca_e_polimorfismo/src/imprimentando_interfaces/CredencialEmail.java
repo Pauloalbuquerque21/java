e96df935959d04f5cd743ae0295503929c7b0eeb
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-25 19:19:46 -0300

    em processo

--- curso_java/section9_heranca_e_polimorfismo/src/imprimentando_interfaces/CredencialEmail.java
+++ curso_java/section9_heranca_e_polimorfismo/src/imprimentando_interfaces/CredencialEmail.java
@@ -2,18 +2,27 @@ package imprimentando_interfaces;
 
 public class CredencialEmail implements Autenticacao {
 
+    private String email;
+    private String senha;
+
+    public CredencialEmail(String email, String senha){
+        this.email = email;
+        this.senha = senha;
+    }
+
+
     @Override
     public String getIdentificacao(){
-        return null;
+        return this.email;
     }
 
     @Override
     public String getSenha(){
-        return null;
+        return this.senha;
     }
 
     @Override
     public boolean isUsaSenha(){
-        return false;
+        return true;
     }
 }
--- curso_java/section9_heranca_e_polimorfismo/src/imprimentando_interfaces/Main.java
+++ curso_java/section9_heranca_e_polimorfismo/src/imprimentando_interfaces/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args){
         Autenticador autenticador = new Autenticador();
 
+        Autenticacao email = new CredencialEmail("email@fulano.com","123");
+
+        boolean isAutenticado = autenticador.autenticar(email);
 
     }
 
