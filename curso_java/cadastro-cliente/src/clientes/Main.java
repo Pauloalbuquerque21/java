00587b79b85095c397b13f1e663a4f0334e5da74
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-12-27 19:48:13 -0300

    em processo, adicionando os campos na tela

--- curso_java/cadastro-cliente/src/clientes/Main.java
+++ curso_java/cadastro-cliente/src/clientes/Main.java
@@ -1,10 +1,13 @@
 package clientes;
 
 import clientes.dominio.Cliente;
+import clientes.apresentacao.TelaCadastro;
 
 public class Main {
     public static void main(String[] args){
         Cliente cliente = new Cliente();
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.setVisible(true);
 
     }
 }
