5182c8881078e7f0bacc9626ae43d562885b45da
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-04 09:32:22 -0300

    em processo com a atividade

--- curso_java/Atividades gpt/cadastro_cliente/TelaCadastro.java
+++ curso_java/Atividades gpt/cadastro_cliente/TelaCadastro.java
@@ -5,17 +5,40 @@ import javax.swing.*;
 public class TelaCadastro extends JFrame{
 
     private JTextField campoNome;
+    private JTextField campoCpf;
+    private JComboBox<TipoSexo> campoSexo;
+    private JButton botaoSalvar;
 
 
     public TelaCadastro(){
+        //Tamanho da janela
         setSize(400,250);
+        //Titulo da janela
         setTitle("Cadastro");
+        //o que acontece, quando o usuári clica no "X" da tela.
+        //Exit_on_close fecha a janela e encerra o programa
+        //Outra opções
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         setLayout(null);
         setVisible(true);
+
+        JLabel labelNome = new JLabel("Nome:");
+        labelNome.setBounds(20,20,80,20);
+        add(labelNome);
+
+        campoNome = new JTextField();
+        campoNome.setBounds(20,40,200,25);
+        add(campoNome);
+
     }
+
+
+
 
     public static void main(String[] args) {
         new TelaCadastro();
     }
+
+
 }
