8ee40cc2126932646d184734695dbbfb898334f5
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-07 22:23:51 -0300

    em processos

---
+++ curso_java/Atividades gpt/cadastro_cliente_arraylist/cadastroCliente.java
@@ -0,0 +1,14 @@
+package cadastro_cliente_arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class cadastroCliente {
+    private List<cliente> clientes;
+
+    public cadastroCliente(){
+        this.clientes = new ArrayList<>();
+    }
+
+}
---
+++ curso_java/Atividades gpt/cadastro_cliente_arraylist/cliente.java
@@ -0,0 +1,39 @@
+package cadastro_cliente_arraylist;
+
+import java.util.UUID;
+
+public class cliente {
+    public UUID codigo;
+    public String nome;
+    public String cpf;
+
+    public cliente(String nome, String cpf){
+        this.codigo = UUID.randomUUID();
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public UUID getCodigo(){
+        return codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ",CPF: "+ cpf + "Código:"+ codigo;
+    }
+
+
+
+
+
+
+}
