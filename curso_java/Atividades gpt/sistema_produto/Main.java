44c94838b72b39dbe0eb2ba2766b5dcb5f2c1d88
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-11 10:49:41 -0300

    em processo da atividadesiste sistema de produto

---
+++ curso_java/Atividades gpt/sistema_produto/Main.java
@@ -0,0 +1,13 @@
+package sistema_produto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<Produto> produto = new ArrayList<>();
+
+
+
+    }
+}
---
+++ curso_java/Atividades gpt/sistema_produto/Produto.java
@@ -0,0 +1,35 @@
+package sistema_produto;
+
+public class Produto {
+    public int codigo;
+    public String nome;
+    public double preco;
+
+    public Produto(int codigo, String nome, double preco){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public String toString() {
+        return "Codigo: " + codigo + "\nNome: " + nome + "\n Preço:" + preco;
+    }
+
+
+
+
+
+}
