a18fbc27cf9cd7f1739e48a29114fc52209c6cac
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-12 07:43:08 -0300

    em processo

--- curso_java/Atividades gpt/sistema_produto/Main.java
+++ curso_java/Atividades gpt/sistema_produto/Main.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List<Produto> produto = new ArrayList<>();
+        List<Produto> produtos = new ArrayList<>();
 
+        produtos.add(new Produto(3,"Teclado",120.00));
+        produtos.add(new Produto(1,"Mouse",80.00));
+        produtos.add(new Produto(5,"Monitor",900.00));
+        produtos.add(new Produto(5,"Cadeira",300.00));
+        produtos.add(new Produto(4,"Notebook",3500.00));
 
 
     }
