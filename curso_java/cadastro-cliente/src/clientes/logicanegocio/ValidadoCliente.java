be136a075a3719dd807b612e5a7895de4402c575
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-18 22:17:04 -0300

    aprendendo tratamento de erro

---
+++ curso_java/cadastro-cliente/src/clientes/logicanegocio/ValidadoCliente.java
@@ -0,0 +1,15 @@
+package clientes.logicanegocio;
+
+import clientes.dominio.Cliente;
+import estudo.exceptions.CpfInvalidoException;
+
+public class ValidadoCliente {
+
+    public static void validar(Cliente cliente) throws CpfInvalidoException{
+        //Esse length mostra o valor da variavel
+        if(cliente.getCpf().length() != 11){
+            throw new CpfInvalidoException("Cpf Inválido!");
+        }
+
+    }
+}
---
+++ curso_java/cadastro-cliente/src/estudo/exceptions/CpfInvalidoException.java
@@ -0,0 +1,7 @@
+package estudo.exceptions;
+
+public class CpfInvalidoException extends Exception {
+    public CpfInvalidoException(String message) {
+        super(message);
+    }
+}
