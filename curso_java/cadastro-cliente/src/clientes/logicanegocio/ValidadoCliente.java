2ca334ad3fe169e85b4f5e3f070cb8f172255961
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-19 09:12:19 -0300

    continuando os estudos de exceptions e tratamento de erros

--- curso_java/cadastro-cliente/src/clientes/logicanegocio/ValidadoCliente.java
+++ curso_java/cadastro-cliente/src/clientes/logicanegocio/ValidadoCliente.java
@@ -4,7 +4,9 @@ import clientes.dominio.Cliente;
 import estudo.exceptions.CpfInvalidoException;
 
 public class ValidadoCliente {
-    //
+    //O throws tem como objetivo criar regras, ou seja, quem chamar esse método validar,
+    //vai ter que tratar, pois vai lançar esse CpfInvalidoException
+    //throw lança a exceção, throws avisa que ela pode acontecer
     public static void validar(Cliente cliente) throws CpfInvalidoException{
         //Esse length mostra o valor da variavel
         if(cliente.getCpf().length() != 11){
