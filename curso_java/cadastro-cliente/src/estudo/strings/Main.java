98a74dce6ebb6d05cdb33c44a1276e212720e23e
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-24 16:27:36 -0300

    iniciando os estudos sobre strings

---
+++ curso_java/cadastro-cliente/src/estudo/strings/Main.java
@@ -0,0 +1,13 @@
+package estudo.strings;
+
+public class Main {
+    static String valor = "A lebre é branca";
+
+    public static void main(String[] args) {
+        tamanhoString();
+    }
+
+    private static void tamanhoString(){
+        int tamanho = valor.length();
+    }
+}
