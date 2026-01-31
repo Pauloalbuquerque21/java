e5ab802705af34228262cb31f2b156ece6382ca1
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-01-31 20:14:36 -0300

    em processo

--- curso_java/Atividades gpt/relatorio_financeiros/Escrita_arquivo.java
+++ curso_java/Atividades gpt/relatorio_financeiros/Escrita_arquivo.java
@@ -1,4 +1,21 @@
 package relatorio_financeiros;
 
+import java.io.File;
+import java.io.FileWriter;
+
 public class Escrita_arquivo {
+
+    public void criarPasta(){
+        File file = new File("C:\\Users\\paulo\\OneDrive\\Documentos\\pastaTest\\sistema")
+        boolean criou = file.mkdir();
+        System.out.println("Pasta croiada" + criou);
+    }
+
+    public void criarArquivo(){
+        File arquivo = new File("C:\\Users\\paulo\\OneDrive\\Documentos\\pastaTest\\vendou_do_dia.txt");
+
+        FileWriter filewriter = new FileWrite
+    }
+
+
 }
