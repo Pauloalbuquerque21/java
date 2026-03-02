d1874424876e8e76522f53c0749fcfc4de9d668c
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-03-02 11:54:57 -0300

    ajustando o programa

--- curso_java/Atividades gpt/Sistema_gestao_jiu_jitsu/Praticante.java
+++ curso_java/Atividades gpt/Sistema_gestao_jiu_jitsu/Praticante.java
@@ -2,20 +2,21 @@ package Sistema_gestao_jiu_jitsu;
 
 public abstract class Praticante {
     public String nome;
-    private int idade;
-    private String faixa;
+    public int idade;
+    public String faixa;
 
     public void exibirDetalhes(){
-        System.out.print("Nome:"+nome+"\nIdade:"idade+"Faixa:"+faixa);
+        System.out.print("Nome:"+nome+"\nIdade:"+idade+"Faixa:"+faixa);
     }
 
 }
 
 public class Aluno extends Praticante implements AptoCompeticao{
     private String nomeProfessor;
+
     @Override
     public void exibirDetalhes(){
-        System.out.print("");
+        System.out.print("Nome: "+nome+"\nIdade: "+idade+"\nFaixa: "+faixa+"\nnomeProfessor: "+nomeProfessor);
     }
 
     public void fazerAquecimento(){
@@ -27,7 +28,7 @@ public class Professor extends Praticante{
     private String grouNaFaixaPreta;
     @Override
     public void exibirDetalhes(){
-        System.out.print("");
+        System.out.print("Nome: "+nome+"\nIdade: "+idade+"\nFaixa: "+faixa+"\nGrau da Faixa:"+grouNaFaixaPreta);
     }
 }
 
