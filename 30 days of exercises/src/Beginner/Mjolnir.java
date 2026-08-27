d294b6ac9a349d2d1a47e369e2a86f7b06de0f49
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2026-08-26 21:21:27 -0300

    atividade mjolnir concluido

---
+++ 30 days of exercises/src/Beginner/Mjolnir.java
@@ -0,0 +1,26 @@
+package Beginner;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Mjolnir {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+        int numberParticipant = Integer.parseInt(in.readLine());
+        for(int i = 0; i < numberParticipant; i++){
+            String nameParticipantAndPower = in.readLine();
+            String[] ArrayParticipantAndPower = nameParticipantAndPower.split(" ");
+            if(ArrayParticipantAndPower[0].toLowerCase().equals("thor")){
+                System.out.println("Y");
+            }else{
+                System.out.println("N");
+            }
+
+        }
+
+
+    }
+}
